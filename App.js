cf6e47eef56d6742156bb1f3e74912bf53612899
Alkin Maystorov <alkin971@gmail.com>
2022-04-16 20:41:57 +0200

    add: card and app text component

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import {} from '@expo/vector-icons';
+
 import AppButton from './app/components/AppButton';
+import Card from './app/components/Card';
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View
+      style={{
+        backgroundColor: '#f8f4f4',
+        padding: 20,
+        paddingTop: 100,
+      }}>
+      <Card
+        title="Red jacket for sale"
+        subTitle="$100"
+        image={require('./app/assets/jacket.jpg')}
+      />
+    </View>
+  );
 }
