b0efe03e7bc474d5c8d53556650ada125acf1b5e
Alkin Maystorov <alkin971@gmail.com>
2022-04-17 19:19:09 +0200

    refactor: list item component

--- App.js
+++ App.js
@@ -7,11 +7,18 @@ import Card from './app/components/Card';
 
 // Screens
 
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
+import ListItem from './app/components/ListItem';
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <ListItem title="My title" ImageComponent={<Icon name="email" />} />
+    </Screen>
+  );
 }
