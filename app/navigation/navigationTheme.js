4b5f3bf24fcb1a7270989ce17eb07c62ae4a645c
Alkin Maystorov <alkin971@gmail.com>
2022-04-27 13:14:12 +0200

    add: navigation theme

---
+++ app/navigation/navigationTheme.js
@@ -0,0 +1,11 @@
+import { DefaultTheme } from '@react-navigation/native';
+import defaultStyles from '../config/defaultStyles';
+
+export default {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: defaultStyles.colors.primary,
+    background: defaultStyles.colors.white,
+  },
+};
