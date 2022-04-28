5cfda3744a123019a2bfb68cf5e303886c99f731
Alkin Maystorov <alkin971@gmail.com>
2022-04-28 14:56:30 +0200

    add: apisauce to make calls to the server, get listings and handle error

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 
 // Styles
@@ -7,28 +7,41 @@ import defaultStyles from '../config/defaultStyles';
 // Components
 import Card from '../components/Card';
 import Screen from '../components/Screen';
+import AppText from '../components/AppText';
+import AppButton from '../components/AppButton';
 
 // Routes
 import routes from '../navigation/routes';
 
-const listings = [
-  {
-    id: 1,
-    title: 'Red jacket for sale',
-    price: 100,
-    image: require('../assets/jacket.jpg'),
-  },
-  {
-    id: 2,
-    title: 'Couch in great condition',
-    price: 500,
-    image: require('../assets/couch.jpg'),
-  },
-];
+// API
+import listingsApi from '../api/listings';
 
 function ListingsScreen({ navigation }) {
+  const [listings, setListings] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadListings = async () => {
+    const response = await listingsApi.getListings();
+
+    if (!response.ok) return setError(true);
+
+    setError(false);
+    setListings(response.data);
+  };
+
+  useEffect(() => {
+    loadListings();
+  }, []);
+
   return (
     <Screen style={styles.screen}>
+      {/* TODO: Style error message */}
+      {error && (
+        <>
+          <AppText>Couldn't retrive the listings.</AppText>
+          <AppButton title="Retry" onPress={loadListings} />
+        </>
+      )}
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
@@ -36,7 +49,7 @@ function ListingsScreen({ navigation }) {
           <Card
             title={item.title}
             subTitle={'$' + item.price}
-            image={item.image}
+            imageUrl={item.images[0].url}
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
           />
         )}
