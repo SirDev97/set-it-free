a30b8b1258c99924d1d50161a794c4ae74f334df
Alkin Maystorov <alkin971@gmail.com>
2022-04-29 12:53:58 +0200

    add: reusable hook to make api calls

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -16,22 +16,15 @@ import routes from '../navigation/routes';
 
 // API
 import listingsApi from '../api/listings';
+import useApi from '../hooks/useApi';
 
 function ListingsScreen({ navigation }) {
-  const [listings, setListings] = useState([]);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const loadListings = async () => {
-    setLoading(true);
-    const response = await listingsApi.getListings();
-    setLoading(false);
-
-    if (!response.ok) return setError(true);
-
-    setError(false);
-    setListings(response.data);
-  };
+  const {
+    data: listings,
+    error,
+    loading,
+    request: loadListings,
+  } = useApi(listingsApi.getListings);
 
   useEffect(() => {
     loadListings();
